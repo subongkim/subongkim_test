be31557099a0d1aa526f63470d93566025629988
DESKTOP-29CCPB7\user <subongkim@lginnotek.com>
2018-04-05 15:11:09 +0900

    [test] 바뀌는지 테스트

--- cppTest/cppTest/move.cpp
+++ cppTest/cppTest/move.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
 
-class Cat
-{
-	char* name;
-	int age;
-public:
-	Cat(const char* n, int a) : age(a)
-	{
-		name = new char[strlen(n) + 1];
-		strcpy(name, n);
-	}
-	~Cat() { delete[] name; }
-};
-
 int main()
 {
-	Cat c1("NABI", 2);
-	Cat c2 = c1;
+	std::cout << "바꿔지나 테스트" << std::endl;
+	return 0;
 }
