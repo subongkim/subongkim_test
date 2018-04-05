0b3b046bc9ed04265f82f4c62afb2a5b1b9b0b62
DESKTOP-29CCPB7\user <subongkim@lginnotek.com>
2018-04-05 17:25:28 +0900

    [test] STL move 예제

--- cppTest/cppTest/move.cpp
+++ cppTest/cppTest/move.cpp
@@ -2,25 +2,41 @@
 using namespace std;
 class Test
 {
-	int* resource;
 public:
 	Test() {}	// 자원할당
 	~Test() {}	// 자원해지
-
-	//복사 생성자 : 깊은복사 or 참조계수 
-	//const lvalue reference이므로 lvalue와 rvalue 모두 받을 수 있음.
 	Test(const Test& t) { cout << "Copy" << endl; }
-
-	// move 생성자 : 소유권 이전(자원 전달)
-	//rvalue만 전달 받을 수 있음.
 	Test(Test&& t)		{ cout << "Move" << endl; }
+
+	Test& operator=(const Test& t)	{
+		{ cout << "Copy=" << endl; }
+		return *this; } // 복사 대입연산자
+	Test& operator=(Test&& t)		{ 
+		{ cout << "Move=" << endl; }
+		return *this; } // move 대입연산자
 };
 
+template<typename T> void Swap(T& x, T& y)
+{
+
+	//	Test tmp = x;	// 복사 생성자
+	//	x = y;			// 복사 대입
+	//	y = tmp;
+	Test tmp = move(x);
+	x = move(y);
+	y = move(tmp);
+}
 int main()
 {
 	Test t1;
-	Test t2 = t1;		// t1은 lvalue이므로 복사 생성자 호출
-	Test t3 = Test();	// 임시객체(rvlaue)이므로 move 생성자 호출
-						// 단 오버로딩 우선순위에 의해 rvalue reference로 감.
+	Test t2 = t1;						// Copy
+	Test t3 = Test();					// Move
+	Test t4 = static_cast<Test&&>(t1);	// Move
+	Test t5 = move(t2);					// Move, move가 내부적으로 위처럼 캐스팅함.
+
+	Test t6;
+	Test t7 = t6;	// 초기화. 복사 생성자
+	t6 = t7;		// 대입.   대입 연산자
+	t6 = move(t7);	// move 대입 연산자
 }
 
