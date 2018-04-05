6fdb1fe16d6729b53bef42951ded013d3c8ac9fb
DESKTOP-29CCPB7\user <subongkim@lginnotek.com>
2018-04-05 17:06:04 +0900

    [test] 이동(move) 생성자 예제 2

--- cppTest/cppTest/move.cpp
+++ cppTest/cppTest/move.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
-
-class Cat
+using namespace std;
+class Test
 {
-	char* name;
-	int age;
+	int* resource;
 public:
-	Cat(const char* n, int a) : age(a)
-	{
-		name = new char[strlen(n) + 1];
-		strcpy(name, n);
-	}
-	~Cat() { delete[] name; }
-	// 깊은 복사로 구현한 복사 생성자
-	Cat(const Cat& c) : age(c.age) // 일반 객체들은 이쪽으로 옴.
-	{
-		name = new char[strlen(c.name) +1];
-		strcpy(name, c.name);
-	}
-	~Cat() { delete[] name; }
-	// 소유권 이전의 이동(move) 생성자
-	Cat(Cat&& c) : age(c.age) // rvalue reference이므로 rvalue만 옴.(임시객체 등)
-	{
-		c.name = nullptr;
-	}
+	Test() {}	// 자원할당
+	~Test() {}	// 자원해지
+
+	//복사 생성자 : 깊은복사 or 참조계수 
+	//const lvalue reference이므로 lvalue와 rvalue 모두 받을 수 있음.
+	Test(const Test& t) { cout << "Copy" << endl; }
+
+	// move 생성자 : 소유권 이전(자원 전달)
+	//rvalue만 전달 받을 수 있음.
+	Test(Test&& t)		{ cout << "Move" << endl; }
 };
-Cat foo() // 값 리턴 -> 임시객체로 리턴(rvalue)
-{
-	Cat cat("NABI", 2);
-	return cat;
-}
 
 int main()
 {
-	Cat c = foo(); //임시객체
+	Test t1;
+	Test t2 = t1;		// t1은 lvalue이므로 복사 생성자 호출
+	Test t3 = Test();	// 임시객체(rvlaue)이므로 move 생성자 호출
+						// 단 오버로딩 우선순위에 의해 rvalue reference로 감.
 }
 
